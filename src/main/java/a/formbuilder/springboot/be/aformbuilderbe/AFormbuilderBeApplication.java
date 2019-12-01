fbe4267ad2ac5fe085af89a671a522caa80691d1
Daniel Lei <dleibox@gmail.com>
2019-12-01 10:23:39 -0500

    Initial commit

---
+++ src/main/java/a/formbuilder/springboot/be/aformbuilderbe/AFormbuilderBeApplication.java
@@ -0,0 +1,13 @@
+package a.formbuilder.springboot.be.aformbuilderbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AFormbuilderBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AFormbuilderBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/a/formbuilder/springboot/be/aformbuilderbe/AFormbuilderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package a.formbuilder.springboot.be.aformbuilderbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AFormbuilderBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
