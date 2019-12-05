37e784930f8f27359f64c0db0100424ffffc1165
Daniel Lei <dleibox@gmail.com>
2019-12-04 22:52:30 -0500

    Updates

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
--- DROP TABLE IF EXISTS form_item;
+DROP TABLE IF EXISTS form_item;
 
--- CREATE TABLE form_item (
---     id BIGINT NOT NULL PRIMARY KEY,
---     name VARCHAR(250) NOT NULL,
---     json VARCHAR(2500000) DEFAULT NULL
--- )
+CREATE TABLE form_item (
+    id BIGINT NOT NULL PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    json VARCHAR(2500000) DEFAULT NULL
+)
